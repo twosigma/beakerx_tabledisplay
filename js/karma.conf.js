f150c58eecdb0a8c0cd5eae80bda93b0520f9786
Łukasz Pior <piorek82@gmail.com>
2020-05-20 12:21:35 +0200

    #1 add karma configuration

---
+++ js/karma.conf.js
@@ -0,0 +1,60 @@
+var webpackConfig =  require('./webpack.config.js');
+var webpack = require('webpack');
+// var path = require('path');
+// const process = require('process');
+
+module.exports = function(config) {
+  config.set({
+    browsers: ['ChromeHeadless'],
+    frameworks: ['mocha', 'karma-typescript'],
+    reporters: ['progress', 'coverage', 'karma-typescript'],
+    files: [
+      { pattern: 'src/**/*.ts' },
+      { pattern: 'test/**/*.ts' },
+      { pattern: 'src/**/*.scss', included: false, watched: false }
+    ],
+    preprocessors: {
+      'src/**/!(Sanitize).ts': ['karma-typescript', "coverage"],
+      'src/**/Sanitize.ts': ['karma-typescript'],
+      'test/**/*.ts': ['karma-typescript'],
+    },
+    karmaTypescriptConfig: {
+      bundlerOptions: {
+        transforms: [require("karma-typescript-es6-transform")()],
+      }
+    },
+    // webpack: {
+    //   mode: 'development',
+    //   module: {
+    //     rules: webpackConfig[2].module.rules.concat({
+    //       test: /\.ts$/,
+    //       use: 'ts-loader'
+    //     }, {
+    //       test: /\.ts$/,
+    //       exclude: [ path.resolve(__dirname, "src", "test") ],
+    //       enforce: 'post',
+    //       use: {
+    //         loader: 'istanbul-instrumenter-loader',
+    //         options: { esModules: true }
+    //       }
+    //     })
+    //   },
+    //   devtool: "inline-source-map",
+    //   resolve: {
+    //     extensions: [".ts"],
+    //   }
+    // },
+    // webpackMiddleware: {
+    //   noInfo: true,
+    // },
+    // coverageIstanbulReporter: {
+    //   reports: [ 'html', 'text-summary', 'lcovonly' ],
+    //   dir: path.join(__dirname, 'coverage'),
+    //   fixWebpackSourcePaths: true,
+    //   'report-config': {
+    //     html: { outdir: 'html' }
+    //   }
+    // },
+    singleRun: true,
+  });
+};
