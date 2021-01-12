cd109d11740fefc2d67b6934e602835e6cfaae57
hbcarlos <carlosherrerocontact@gmail.com>
2021-01-12 15:51:04 +0100

    Color variables

--- js/src/utils/Theme.ts
+++ js/src/utils/Theme.ts
@@ -45,6 +45,8 @@ const defaults: { [keys: string]: string; } = {
   '--jp-ui-inverse-font-color2': '#ffffff8C',
   '--jp-ui-inverse-font-color3': '#ffffff66',
 
+  '--jp-rendermime-table-row-background': '#F5F5F5',
+
   '--jp-brand-color0': '#455a64',
   '--jp-brand-color1': '#607d8b',
   '--jp-brand-color2': '#90a4ae',
@@ -83,95 +85,103 @@ const defaults: { [keys: string]: string; } = {
 
 };
 
-export function formatColor(color: string): string {
-  const match = color.match(/rgba\((\d+),\s?(\d+),\s?(\d+),\s?(\d+\.?\d*)\)/);
-  if (match) {
-    const r: number = Math.ceil(Number.parseInt(match[1], 10));
-    const g: number = Math.ceil(Number.parseInt(match[2], 10));
-    const b: number = Math.ceil(Number.parseInt(match[3], 10));
-    const a: number = Math.ceil(Number.parseInt(match[4], 10));
-    const hexColor = CommonUtils.rgbaToHex(r, g, b, a);
-    return hexColor;
-  } else {
-    return color;
-    
+export function formatColor(color: string, factor = 1): string {
+  const rgba = color.match(/rgba\((\d+),\s?(\d+),\s?(\d+),\s?(\d+\.?\d*)\)/);
+  if (rgba) {
+    const r: number = Math.ceil(Number.parseInt(rgba[1], 10));
+    const g: number = Math.ceil(Number.parseInt(rgba[2], 10));
+    const b: number = Math.ceil(Number.parseInt(rgba[3], 10));
+    const a: number = Math.ceil(Number.parseInt(rgba[4], 10)) * factor;
+    return CommonUtils.rgbaToHex(r, g, b, a);
+  } 
+  
+  const rgb = color.match(/rgb\((\d+),\s?(\d+),\s?(\d+)\)/);
+  if (rgb) {
+    const r: number = Math.ceil(Number.parseInt(rgba[1], 10)) * factor;
+    const g: number = Math.ceil(Number.parseInt(rgba[2], 10)) * factor;
+    const b: number = Math.ceil(Number.parseInt(rgba[3], 10)) * factor;
+    return CommonUtils.rgbaToHex(r, g, b);
   }
+
+  const hex = color.match(/#(\w{2}|\w)?(\w{2}|\w)?(\w{2}|\w)?(\w{2}|\w)?/);
+  if (factor !== 1 && hex) {
+    if (hex[4]) {
+      const r: number = parseInt(hex[1] || '00', 16);
+      const g: number = parseInt(hex[2] || '00', 16);
+      const b: number = parseInt(hex[3] || '00', 16);
+      const a: number = parseInt(hex[4] || '00', 16) * factor;
+      return CommonUtils.rgbaToHex(r, g, b, a);
+    } else {
+      const r: number = parseInt(hex[1] || '00', 16) * factor;
+      const g: number = parseInt(hex[2] || '00', 16) * factor;
+      const b: number = parseInt(hex[3] || '00', 16) * factor;
+      return CommonUtils.rgbaToHex(r, g, b);
+    }
+  }
+
+  return color;
 }
 
-export function evaluateCSSVariable(name: string) {
+export function evaluateCSSVariable(name: string, factor = 1): string {
   const value = window.getComputedStyle(document.documentElement).getPropertyValue(name);
   if (value) {
-    return formatColor(value);
+    return formatColor(value, factor);
   } else {
     return defaults[name];
   }
 }
 
 export class Theme {
+
+  public static updateStyle(): void {
+    // Header color
+    this.DEFAULT_HEADER_FONT_COLOR = evaluateCSSVariable('--jp-ui-font-color1');
+    this.DEFAULT_HEADER_BACKGROUND = evaluateCSSVariable('--jp-layout-color3');
+    this.DEFAULT_COLOR = '';
+
+    // Cell color
+    this.DEFAULT_DATA_FONT_COLOR = evaluateCSSVariable('--jp-ui-font-color0');
+    this.DEFAULT_CELL_BACKGROUND = evaluateCSSVariable('--jp-rendermime-table-row-background');
+    this.FOCUSED_CELL_BACKGROUND = evaluateCSSVariable('--jp-accent-color2');
+    this.SELECTED_CELL_BACKGROUND = evaluateCSSVariable('--jp-brand-color1');
+    this.DATA_BARS_COLOR = evaluateCSSVariable('--md-blue-A200');
+    this.DEFAULT_HIGHLIGHT_COLOR = evaluateCSSVariable('--jp-warn-color0');
+    this.HIGHLIGHTED_CELL_BACKGROUND_EVEN = evaluateCSSVariable('--jp-success-color1');
+    this.HIGHLIGHTED_CELL_BACKGROUND_ODD = evaluateCSSVariable('--jp-info-color1');
+    this.MIN_LIGHTNESS_VALUE = 25;
+    this.MIN_SATURATION_VALUE = 25;
+  }
+
   public static getStyle(): DataGrid.Style {
     return {
       ...DataGrid.defaultStyle,
       voidColor: evaluateCSSVariable('--jp-layout-color0'),
       backgroundColor: evaluateCSSVariable('--jp-layout-color1'),
-      rowBackgroundColor: (i) => (i % 2 === 0 ? evaluateCSSVariable('--jp-layout-color1') : evaluateCSSVariable('--jp-layout-color2')),
+      rowBackgroundColor: (i) => {
+        return i % 2 === 0 ? 
+          evaluateCSSVariable('--jp-rendermime-table-row-background') :
+          evaluateCSSVariable('--jp-rendermime-table-row-background', 0.7);
+      },
       gridLineColor: evaluateCSSVariable('--jp-border-color3'),
-      
       headerBackgroundColor: evaluateCSSVariable('--jp-layout-color3'),
       headerGridLineColor: evaluateCSSVariable('--jp-border-color3'),
     };
   }
 
   // Header color
-  public static get DEFAULT_HEADER_FONT_COLOR(): string {
-    return evaluateCSSVariable('--jp-ui-font-color1');
-  }
-
-  public static get DEFAULT_HEADER_BACKGROUND(): string {
-    return evaluateCSSVariable('--jp-layout-color3');
-  }
-
-  public static get DEFAULT_COLOR(): string {
-    return '';
-  }
+  public static DEFAULT_HEADER_FONT_COLOR: string = evaluateCSSVariable('--jp-ui-font-color1');
+  public static DEFAULT_HEADER_BACKGROUND: string = evaluateCSSVariable('--jp-layout-color3');
+  public static DEFAULT_COLOR: string = '';
 
   // Cell color
-  public static get DEFAULT_DATA_FONT_COLOR(): string {
-    return evaluateCSSVariable('--jp-ui-font-color0');
-  }
-
-  public static get DEFAULT_CELL_BACKGROUND(): string {
-    return evaluateCSSVariable('--jp-layout-color1');
-  }
-
-  public static get FOCUSED_CELL_BACKGROUND(): string {
-    return evaluateCSSVariable('--jp-accent-color2');
-  }
-
-  public static get SELECTED_CELL_BACKGROUND(): string {
-    return evaluateCSSVariable('--jp-info-color0');
-  }
-
-  public static get DATA_BARS_COLOR(): string {
-    return evaluateCSSVariable('--md-blue-A200');
-  }
-
-  public static get DEFAULT_HIGHLIGHT_COLOR(): string {
-    return evaluateCSSVariable('--jp-warn-color1');
-  }
-
-  public static get HIGHLIGHTED_CELL_BACKGROUND_EVEN(): string {
-    return evaluateCSSVariable('--jp-success-color1');
-  }
-
-  public static get HIGHLIGHTED_CELL_BACKGROUND_ODD(): string {
-    return evaluateCSSVariable('--jp-info-color1');
-  }
-
-  public static get MIN_LIGHTNESS_VALUE(): number {
-    return 25;
-  }
-
-  public static get MIN_SATURATION_VALUE(): number {
-    return 25;
-  }
+  public static DEFAULT_DATA_FONT_COLOR: string = evaluateCSSVariable('--jp-ui-font-color0');
+  public static DEFAULT_CELL_BACKGROUND: string = evaluateCSSVariable('--jp-rendermime-table-row-background');
+  public static FOCUSED_CELL_BACKGROUND: string = evaluateCSSVariable('--jp-accent-color2');
+  public static SELECTED_CELL_BACKGROUND: string = evaluateCSSVariable('--jp-brand-color1');
+  public static DATA_BARS_COLOR: string = evaluateCSSVariable('--md-blue-A200');
+  public static DEFAULT_HIGHLIGHT_COLOR: string = evaluateCSSVariable('--jp-warn-color0');
+  public static HIGHLIGHTED_CELL_BACKGROUND_EVEN: string = evaluateCSSVariable('--jp-success-color1');
+  public static HIGHLIGHTED_CELL_BACKGROUND_ODD: string = evaluateCSSVariable('--jp-info-color1');
+  public static MIN_LIGHTNESS_VALUE: number = 25;
+  public static MIN_SATURATION_VALUE: number = 25;
 }
